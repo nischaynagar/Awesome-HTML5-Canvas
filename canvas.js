0bb837d25de1edac0ba952714e7ac5c7e496c3bc
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2020-09-02 19:44:08 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -6,37 +6,6 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-// c.fillRect(400, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-// c.fillRect(400, 300, 100, 100);
-// console.log(canvas);
-
-//Line
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = "#fa34a3";
-//line will not appear untill we call the stroke method
-// c.stroke();
-
-//Arc / Circle
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI*2,false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-
-// for(var i=0;  i<3; i++){
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI*2,false);
-//     c.strokeStyle = 'blue';
-//     c.stroke();
-// }
 
     var mouse = {
         x: undefined,
@@ -114,11 +83,7 @@ var c = canvas.getContext('2d');
     
     }
     
-// var x = Math.random() * innerWidth;
-// var y = Math.random() * innerHeight;
-// var dx = (Math.random() - 0.5) * 8;
-// var dy = (Math.random() - 0.5) * 8;
-// var radius = 30;
+
 
 
 var circleArray = [];
